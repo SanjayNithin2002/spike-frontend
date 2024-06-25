ef814a27bc523f431f91d4f767f1af7fc74e4379
Sanjay Nithin <sanjayfornewsletter@gmail.com>
2024-06-25 14:28:50 +0530

    navigation fixed

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import Signup from './components/Signup';
+import Login from './components/Login';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" exact element={<Navigate to="/signup" />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </Router>
+
+  );
+}
+
+export default App;
